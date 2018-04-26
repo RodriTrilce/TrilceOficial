70bfe5bc4ac4d5603357ed38a137e17519abd69d
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-04-26 12:54:50 -0500

    Add preload in link tag Home,Academia:index,404 page, 2

--- resources/views/academia/layouts/template.blade.php
+++ resources/views/academia/layouts/template.blade.php
@@ -7,7 +7,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}" type="image/x-icon" />
-  <link rel="preload" href="{{ mix('/css/others/_icons.css') }}">
+  <link rel="preload" href="{{ mix('/css/others/_icons.css') }}" as="style">
   <link rel="stylesheet" href="{{ mix('/css/main_academia.css') }}">
 </head>
 <body>

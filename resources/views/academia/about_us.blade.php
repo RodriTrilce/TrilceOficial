1e09b8c508411a2a754df1ca5cfdf7a4bef17a50
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-05-08 15:49:39 -0500

    fix 2

--- resources/views/academia/about_us.blade.php
+++ resources/views/academia/about_us.blade.php
@@ -41,7 +41,7 @@
         <div class="col-xs-12 col-sm-2 col-md">
           <div></div>
           <h2>El primer Colegio</h2>
-          <p>En el año 1996, Trilce fundó el primer colegio preuniversitario del Perú en uno de sus locales, ubicado en Miraflores. Con esto, Trilce confirmó su liderazgo y excelencia académica con un método de enseñanza escolar totalmente innovador-</p>
+          <p>En el año 1996, Trilce fundó el primer colegio preuniversitario del Perú en uno de sus locales, ubicado en Miraflores. Con esto, Trilce confirmó su liderazgo y excelencia académica con un método de enseñanza escolar totalmente innovador.</p>
         </div>
         
         <div class="col-xs-12 col-sm-2 col-md">

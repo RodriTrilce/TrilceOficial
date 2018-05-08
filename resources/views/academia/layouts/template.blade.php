0facaa07829db855103248b15dc201d65b28c674
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-05-08 15:10:33 -0500

    fix header&footer

--- resources/views/academia/layouts/template.blade.php
+++ resources/views/academia/layouts/template.blade.php
@@ -18,10 +18,10 @@
 
   <header>
     <div class="row between-xs">
-      <div class="col-xs-6 logo">
+      <div class="col-xs-6 col-sm logo">
         <a href="/academia/"><img src="{{{ asset('static/images/academia/logo.png') }}}" alt="Trilce"></a>
       </div>
-      <div class="row col-xs-6 end-xs pre-nav">
+      <div class="row col-xs-6 col-sm-6 col-md-8 end-xs pre-nav">
           <div class="col-xs-12 nav-info">
               <span><a href="/"><i class="fa fa-home"></i></a> | <a href="http://intranet.trilce.edu.pe" target="_blank">Intranet para alumnos</a> | Call center: <b>6198100</b></span>
           </div>

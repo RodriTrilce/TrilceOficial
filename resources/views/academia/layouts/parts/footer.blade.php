72843e1486b332d8aafcdda8850b9dffacc9a67e
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-05-08 16:25:39 -0500

    Footer fix final

--- resources/views/academia/layouts/parts/footer.blade.php
+++ resources/views/academia/layouts/parts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-  <div class="row">
+  <div class="row col-xs-12 center-xs">
     <div class="row col-xs-12 center-xs footer-top">
       <div class="col-xs-12 col-sm-5 start-xs">
         <span>Usted está aquí: <b>Academia</b></span>
@@ -7,8 +7,8 @@
       <div class="col-sm-6 xs-hide"></div>
     </div>
     
-    <div class="row col-xs-12 center-xs footer-menu">
-      <div class="col-xs-5 start-xs links">
+    <div class="row col-xs-11 center-xs footer-menu">
+      <div class="col-xs start-xs links">
         <ul>
             <li><a href="/colegio">Trilce Colegio</a></li>
             <li><a href="/academia">Trilce Academia</a></li>
@@ -17,7 +17,7 @@
             <li><a href="#">Trabaja con nosotros</a></li>
         </ul>
       </div>
-      <div class="col-xs-6 end-xs">
+      <div class="col-xs-3 end-xs">
           <ul class="footer-right-icons">
             <li><a href="https://api.whatsapp.com/send?phone=51922336565&text=Me%20gustar%C3%ADa%20obtener%20m%C3%A1s%20informaci%C3%B3n" title="Chatea con nosotros" target="_blank" rel="nofollow"><i class="fa fa-whatsapp"></i></a></li>
             <li><a href="https://twitter.com/TRILCEtweet" title="Siguenos en Twitter" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i></a></li>

79bd7304be91eb070baee4cafc5a2207261b3951
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-05-16 10:01:41 -0500

    Colegio: Advance index

--- resources/views/colegio/index.blade.php
+++ resources/views/colegio/index.blade.php
@@ -29,21 +29,21 @@
     <div class="row center-xs">
       <div class="col-xs-9 col-sm-11 col-md-10 start-xs nuevos-i-title">
         <div class="box">
-          <h2>Admisión 2018</h2>
-          <p>Elija la preparación universitaria, exclusiva que está buscando</p>
+          <h2>Admisión 2019</h2>
+          <p>Elija la modalidad que más se adecue a tus necesidades. ¡Alumnos mejor preparados!</p>
         </div>
       </div>
       
-      <div class="row nuevos-i-box col-xs-12 col-sm-11 col-md-10 col-lg-9 between-xs center-sm center-md center-lg">
+      <div class="row col-xs-12 col-sm-11 col-md-10 col-lg-9 between-xs center-sm center-md center-lg nuevos-i-box">
         <div class="col-xs">
           <a href="/academia/preparacion-uni">
             <div class="nuevos-i-top">
               <img src="{{ url('static/images/colegio/ico-alumno-nuevo.jpg') }}" alt="UNI">
-              <h3>Alumno <span class="modality">Nuevo</span></h3>
+              <h3>Alumno <span class="modality modality-blue">Nuevo</span></h3>
             </div>
             <div class="nuevos-i-bottom">
-              <hr>
-              <span>Más información <i class="fa fa-search"></i></span>
+              <span>Leer más <i class="fa fa-search"></i></span>
+              <div class="nuevos-hr nuevos-hr-blue"></div>
             </div>
           </a>
         </div>
@@ -51,11 +51,11 @@
           <a href="/academia/preparacion-san-marcos">
             <div class="nuevos-i-top">
               <img src="{{ url('static/images/colegio/ico-alumno-regular.jpg') }}" alt="UNMSM">
-              <h3>Alumno <span class="modality">Traslado</span></h3>
+              <h3>Alumno <span class="modality modality-yellow">Traslado</span></h3>
             </div>
             <div class="nuevos-i-bottom">
-              <hr>
-              <span>Más información <i class="fa fa-search"></i></span>
+              <span>Leer más <i class="fa fa-search"></i></span>
+              <div class="nuevos-hr nuevos-hr-yellow"></div>
             </div>
           </a>
         </div>
@@ -63,11 +63,11 @@
           <a href="/academia/preparacion-pucp">
             <div class="nuevos-i-top">
               <img src="{{ url('static/images/colegio/ico-alumno-traslado.jpg') }}" alt="PUCP">
-              <h3>Alumno <span class="modality">Regular</span></h3>
+              <h3>Alumno <span class="modality modality-orange">Regular</span></h3>
             </div>
             <div class="nuevos-i-bottom">
-              <hr>
-              <span>Más información <i class="fa fa-search"></i></span>
+              <span>Leer más <i class="fa fa-search"></i></span>
+              <div class="nuevos-hr nuevos-hr-orange"></div>
             </div>
           </a>
         </div>

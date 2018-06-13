b39999e17ff8061b79a741e977bbf55c92be018d
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-06-13 17:22:59 -0500

    Changes

--- resources/views/colegio/partials/admission/regular.blade.php
+++ resources/views/colegio/partials/admission/regular.blade.php
@@ -1,6 +1,6 @@
 <div class="row col-xs-12 center-xs admission-new-title xs-hide">
   <div class="col-xs-12">
-    <h2 class="new-t-title">Alumnos Traslado</h2>
+    <h2 class="new-t-title">Alumnos Regular</h2>
     <h3 class="new-t-subtitle">Periodo: Julio 2018 a Abril 2019</h3>
   </div>
   <div class="col-xs-12 col-sm-4 new-t-p">
@@ -10,7 +10,7 @@
 
 <div class="row col-xs-12 center-xs container-base admission-new-table admission-transfer-table">
   <div class="row col-xs-12 center-xs">
-    <div class="col-xs-12">    <h3 class="admission-regular-title">Inscripción</h3></div>
+    <div class="col-xs-12">  <h3 class="admission-regular-title">Inscripción</h3></div>
     <div style="max-width:500px; text-align:left;">
       <ul>
         <li>Actualizar ficha de matrícula Trilce 2019.</li>

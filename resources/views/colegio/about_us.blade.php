0bc602d198eab1933abbafe96ebf80886aab9710
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-06-14 16:28:23 -0500

    About changes

--- resources/views/colegio/about_us.blade.php
+++ resources/views/colegio/about_us.blade.php
@@ -17,7 +17,7 @@
     <div class="row col-xs-12 col-sm-9 col-md-8 start-xs start-sm start-md">
 
       <div class="row col-xs-12 about-history">
-        <div class="col-xs-12 xs-hide col-sm-3 about-history--left">
+        <div class="row col-xs-12 xs-hide col-sm-3 about-history--left">
           <img src="/static/images/colegio/about-image-01.jpg" alt="Sobre nosotros">
         </div>
         <div class="col-xs-12 col-sm about-history--right">

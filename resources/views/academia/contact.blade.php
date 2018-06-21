a60734b914dfdb1678538682fabf120c0eb23611
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-06-21 16:04:59 -0500

    Academia, Colegio: Form contact fix 2

--- resources/views/academia/contact.blade.php
+++ resources/views/academia/contact.blade.php
@@ -85,9 +85,11 @@
                 <div class="g-recaptcha" data-callback="captcha" data-sitekey="6LeHW1cUAAAAALgptl-KjhXhIlK5JzCBI4Mt0Zi1"></div>
               </div>
             </div>
-            <br>
-            <input type="text" name="contact_type" value="academia" hidden readonly>
-            <button type="submit" name="contact_validate" id="contact_validate" class="validate">Enviar <i class="fa fa-check"></i></button>
+            
+            <div class="col-xs-12">
+              <input type="text" name="contact_type" value="academia" hidden readonly>
+              <button type="submit" name="contact_validate" id="contact_validate" class="validate">Enviar <i class="fa fa-check"></i></button>
+            </div>
           </form>
         </div>
 

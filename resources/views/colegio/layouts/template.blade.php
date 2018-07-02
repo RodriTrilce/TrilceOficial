3e0021dfc601329253d8dc5d59ad3a771b653008
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-07-02 16:20:30 -0500

    Add cache removed

--- resources/views/colegio/layouts/template.blade.php
+++ resources/views/colegio/layouts/template.blade.php
@@ -7,8 +7,8 @@
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}" type="image/x-icon" />
-  <link rel="stylesheet" href="{{ mix('/css/main.css') }}">
-  <link rel="stylesheet" href="{{ mix('/css/main_colegio.css') }}">
+  <link rel="stylesheet" href="{{ mix('/css/main.css') }}?{{rand(0,999999)}}">
+  <link rel="stylesheet" href="{{ mix('/css/main_colegio.css') }}?{{rand(0,999999)}}">
 </head>
 <body>
 

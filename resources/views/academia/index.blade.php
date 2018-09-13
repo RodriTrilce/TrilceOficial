4bd967b042bf051b8d5c8985545556dc97eb0958
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-09-13 14:52:17 -0500

    Academia: changes

--- resources/views/academia/index.blade.php
+++ resources/views/academia/index.blade.php
@@ -68,7 +68,7 @@
     <div class="row center-xs">
       <div class="col-xs-9 col-sm-11 col-md-10 start-xs nuevos-i-title">
         <div class="box">
-          <h2>Nuevos inicios</h2>
+          <h2>Inicios</h2>
           <p>Elija la preparación universitaria, exclusiva que está buscando</p>
         </div>
       </div>

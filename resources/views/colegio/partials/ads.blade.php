a4cd94ac9e0211fc7c1506460300c8fa2e439e23
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-10-05 09:49:12 -0500

    Banner

--- resources/views/colegio/partials/ads.blade.php
+++ resources/views/colegio/partials/ads.blade.php
@@ -1,9 +1,11 @@
 <div class="row col-xs-12 center-xs admission-ads">
-    <a href="http://www.trilce.edu.pe/colegios/inscripcion-admision2019/" target="_blank" class="admission-ads__link">
         <div class="admission-ads__container">
-          <h1>¿Listo para la matrícula?</h1>
-          <button class="admission-ads__container-cta banner__button-cta banner__button-cta--white" type="button" name="button">Registrate</button>
-          <h2>también puede llamarnos 01 619 8100</h2>
+          <a href="http://www.trilce.edu.pe/colegios/inscripcion-admision2019/" target="_blank" class="admission-ads__link">
+            <h1>¿Quieres saber más sobre nuestro proceso de admisión {{date("Y")+1}}?</h1>
+            <button class="admission-ads__container-cta banner__button-cta banner__button-cta--white" type="button" name="button">
+              ¡Regístrate aquí y te daremos toda la información!
+            </button>
+            <h2>O comunícate con nosotros al 619 8100</h2>
+          </a>
         </div>
-    </a>
 </div>

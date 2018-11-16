890da97a4091f39991a33987ef4e324aaff66126
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-11-16 09:13:48 -0500

    Masivo: add command to console kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,8 @@ class Kernel extends ConsoleKernel
 
 //        $schedule->command('command:testmail')->everyMinute();
 //        $schedule->command('command:eco')->everyMinute();
-      $schedule->command('command:sendmailbeca')->everyMinute();
+//        $schedule->command('command:sendmailbeca')->everyMinute();
+          $schedule->command('command:sendmailpucp')->everyMinute();
     }
 
     /**

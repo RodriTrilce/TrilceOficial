88748177683c768aac1f480b3c92f363664acb9c
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-12-13 10:46:43 -0500

    Academia: move preinscripcion in header

--- resources/views/academia/layouts/parts/header.blade.php
+++ resources/views/academia/layouts/parts/header.blade.php
@@ -77,9 +77,9 @@
                 </ul>
               </li>
 
-              <li class=""><a href="/academia/#inicios">Ciclos</a></li>
+              <!--              <li class=""><a href="/academia/preinscripcion" class="{{ (Request::is('academia/preinscripcion')?'nav-item-active nav-item-noclick':'') }}">Preinscripción</a></li> -->
 
-<!--              <li class=""><a href="/academia/preinscripcion" class="{{ (Request::is('academia/preinscripcion')?'nav-item-active nav-item-noclick':'') }}">Preinscripción</a></li> -->
+              <li class=""><a href="/academia/#inicios">Ciclos</a></li>
               <li class=""><a href="/academia/blog" class="{{ (Request::is('academia/blog*')?'nav-item-active':'') }}">Blog</a></li>
               <li class=""><a href="/academia/contacto" class="{{ (Request::is('academia/contacto')?'nav-item-active nav-item-noclick':'') }}">Contáctanos</a></li>
             </ul>

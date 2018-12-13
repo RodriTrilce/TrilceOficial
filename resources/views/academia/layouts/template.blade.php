5a4d0bcad5bd3aa35c5a19c7ad43132562d85445
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-12-13 10:31:29 -0500

    Add fix css charge

--- resources/views/academia/layouts/template.blade.php
+++ resources/views/academia/layouts/template.blade.php
@@ -12,8 +12,8 @@
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}" type="image/x-icon" />
-  <link rel="stylesheet" href="{{ mix('/css/main.css') }}">
-  <link rel="stylesheet" href="{{ mix('/css/main_academia.css') }}">
+  <link rel="stylesheet" href="{{ mix('/css/main.css?v1') }}">
+  <link rel="stylesheet" href="{{ mix('/css/main_academia.css?v1') }}">
   @include('tracking.in_head')
 </head>
 <body>

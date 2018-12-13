a1d6ac83618559d2ac7599542c28446669a356f6
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-12-13 10:09:17 -0500

    Modal fix

--- resources/views/colegio/partials/modal/ads.blade.php
+++ resources/views/colegio/partials/modal/ads.blade.php
@@ -1,7 +1,9 @@
 
   <div id="modal-ads" style="display:none;">
       <div class="row top-xs col-xs-12 center-xs">
-        <img class="xs-hide" src="/storage/static/images/other/pop-up/pop-up-cole.png" alt="Matrícula 2019">
-        <img class="xs-reverse" src="/storage/static/images/other/pop-up/pop-up-cole-movil.png" alt="Matrícula 2019">
+        <a href="/matricula-2019/" class="row top-xs col-xs-12 center-xs">
+          <img class="xs-hide" src="/storage/static/images/other/pop-up/pop-up-cole.png" alt="Matrícula 2019">
+          <img class="xs-reverse" src="/storage/static/images/other/pop-up/pop-up-cole-movil.png" alt="Matrícula 2019">
+        </a>
       </div>
   </div>

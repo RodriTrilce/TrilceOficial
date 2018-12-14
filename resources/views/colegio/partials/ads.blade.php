111e3281ed2c9241399fdf98a96a71639989a2c7
Franco Salcedo <fsalcedo@trilce.edu.pe>
2018-12-14 09:41:37 -0500

    Colegio: fix ads remove target black

--- resources/views/colegio/partials/ads.blade.php
+++ resources/views/colegio/partials/ads.blade.php
@@ -1,6 +1,6 @@
 <div class="row col-xs-12 center-xs admission-ads">
         <div class="admission-ads__container">
-          <a href="/matricula-2019/" target="_blank" class="admission-ads__link">
+          <a href="/matricula-2019/" target="" class="admission-ads__link">
             <h1>¿Quieres saber más sobre nuestro proceso de matrícula 2019?</h1>
             <button class="admission-ads__container-cta banner__button-cta banner__button-cta--white" type="button" name="button">
               Regístrate aquí

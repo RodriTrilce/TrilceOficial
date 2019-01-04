feeef5cf8eb50b8c4bff26e622993b43ce35c6b3
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-01-04 09:29:39 -0500

    Colegio: vacaciones fix maranga fecha

--- resources/views/colegio/partials/vacations/venues/maranga.blade.php
+++ resources/views/colegio/partials/vacations/venues/maranga.blade.php
@@ -353,7 +353,7 @@
 
 <div class="col-xs-12 vacations__bottom vacations__bottom--orange">
   <div>
-    <h1><span><i class="fa fa-lightbulb-o"></i> Inicios: 4, 7 y 8 de enero</span></h1>
+    <h1><span><i class="fa fa-lightbulb-o"></i> Inicios: 4, 7 y 9 de enero</span></h1>
   </div>
   <div>
     <h3><span>Fin: 21 de febrero</span></h3>

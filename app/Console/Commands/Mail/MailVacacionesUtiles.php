57181a597a6a5b7de981decc3a6985ba2d43244c
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-01-08 11:57:09 -0500

    Mail: vacaciones driver add

--- app/Console/Commands/Mail/MailVacacionesUtiles.php
+++ app/Console/Commands/Mail/MailVacacionesUtiles.php
@@ -46,7 +46,7 @@ class MailVacacionesUtiles extends Command
         $users = MailVacacionesUtilesModel::where('send1', '=', '0')->limit(5)->get();
 
         foreach ($users as $user) {
-          $count = Listdriver::where('id', '=', '7')->first();
+          $count = Listdriver::where('id', '=', '9')->first();
 
 /*
           if(($count->count % 100) == 0

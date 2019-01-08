d373a06b0f733213f930cecf2911a7c89a956798
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-01-08 11:13:54 -0500

    Colegio: vacaciones agregando Últimas vacantes en vez de fechas

--- resources/views/colegio/partials/vacations/venues/chorrillos.blade.php
+++ resources/views/colegio/partials/vacations/venues/chorrillos.blade.php
@@ -596,7 +596,7 @@
 
 <div class="col-xs-12 vacations__bottom vacations__bottom--orange">
   <div>
-    <h1><span><i class="fa fa-lightbulb-o"></i> Inicios: 7 y 8 de enero</span></h1>
+    <h1><span><i class="fa fa-lightbulb-o"></i> Últimas vacantes</span></h1>
   </div>
   <div>
     <h3><span>Fin: 21 de febrero</span></h3>

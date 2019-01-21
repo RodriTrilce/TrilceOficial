92375b3030d7968b971d61fa9a68c98c30b24f93
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-01-21 09:52:27 -0500

    Colegio: añadido refresh a imagenes del popup

--- resources/views/colegio/partials/modal/ads.blade.php
+++ resources/views/colegio/partials/modal/ads.blade.php
@@ -2,8 +2,8 @@
   <div id="modal-ads" style="display:none;">
       <div class="row top-xs col-xs-12 center-xs">
         <a href="/matricula-2019/" class="row top-xs col-xs-12 center-xs">
-          <img class="xs-hide" src="/storage/static/images/other/pop-up/pop-up-cole.png?v2" alt="Matrícula 2019">
-          <img class="xs-reverse" src="/storage/static/images/other/pop-up/pop-up-cole-movil.png?v2" alt="Matrícula 2019">
+          <img class="xs-hide" src="/storage/static/images/other/pop-up/pop-up-cole.png?v3.1" alt="Matrícula 2019">
+          <img class="xs-reverse" src="/storage/static/images/other/pop-up/pop-up-cole-movil.png?v3.1" alt="Matrícula 2019">
         </a>
       </div>
   </div>

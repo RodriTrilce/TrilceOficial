787d85d60f232a9af92c2709274abc5ee4305aee
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-01-23 08:58:03 -0500

    Modal: actualizar el cache ?v

--- resources/views/academia/partials/modal/ads.blade.php
+++ resources/views/academia/partials/modal/ads.blade.php
@@ -2,8 +2,8 @@
     <div id="modal-ads" style="display:none;">
         <div class="row top-xs col-xs-12 center-xs">
           <a href="/inscripcion-academia/" class="row top-xs col-xs-12 center-xs">
-            <img class="xs-hide" src="/storage/static/images/other/pop-up/pop-up-academia.png?v2" alt="No te quedes sin vacante">
-            <img class="xs-reverse" src="/storage/static/images/other/pop-up/pop-up-academia-movil.png?v2" alt="No te quedes sin vacante">
+            <img class="xs-hide" src="/storage/static/images/other/pop-up/pop-up-academia.png?v2.1" alt="No te quedes sin vacante">
+            <img class="xs-reverse" src="/storage/static/images/other/pop-up/pop-up-academia-movil.png?v2.1" alt="No te quedes sin vacante">
           </a>
         </div>
     </div>

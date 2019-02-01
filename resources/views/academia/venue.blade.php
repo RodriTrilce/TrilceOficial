e2e79e7bd794334087e7fd5602a39ccc3ce8f7dd
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-02-01 08:59:20 -0500

    Api key update maps

--- resources/views/academia/venue.blade.php
+++ resources/views/academia/venue.blade.php
@@ -107,7 +107,7 @@
     page = 'venue';
 
   @parent
-  <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDU9IczA6fujtDzHPy0xWnHLqHHMuB1vKM" charset="utf-8"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyD1wH_2k0jIxbUwc1TUI40RANtmuMIbMU8" charset="utf-8"></script>
   <script type="text/javascript">
   function initMap(){
       var options = {

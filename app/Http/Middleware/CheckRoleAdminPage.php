c0e1474ebae7cfe27b47be1b13e09b2d49830b40
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-02-08 11:06:46 -0500

    Admin. roles actuaalziacion

--- app/Http/Middleware/CheckRoleAdminPage.php
+++ app/Http/Middleware/CheckRoleAdminPage.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Route;
-use App\Models\Admin\AdminPageModel;
 use Auth;
 
 class CheckRoleAdminPage
@@ -33,30 +32,6 @@ class CheckRoleAdminPage
         if(!$passed)
           abort(403, "No tienes autorización para ingresar.");
 
-/*
-        $route = explode('.', Route::currentRouteName());
-        $route = $route[0];
-
-        $c = Auth::user()->roles;
-        foreach ($c as $key) {
-            $id = $key->id;
-            $v = AdminPageModel::where('role_id', $id);
-            
-            dd($v);
-
-        }
-
-        die;
-
-
-/*
-        echo Route::currentRouteName();
-        echo "<br>\n";
-
-        dd($request->User()->roles);
-
-        die;
-*/        
         return $next($request);
     }
 }

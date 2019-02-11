1f11559b56bbe05b917dd629737014e6f455a186
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-02-11 11:27:08 -0500

    Academia: añadir link de sorteo en nav

--- resources/views/academia/layouts/parts/header.blade.php
+++ resources/views/academia/layouts/parts/header.blade.php
@@ -20,6 +20,7 @@
                 <div><a class="nav-info-colegio__phone" href="tel:016198100">Call center: <b>6198 100</b></a></div>
               </li>
               <li class="nav__highlight"><a href="/colegio">Colegio</a></li>
+
               <li class=""><a href="/academia/#inicios">Ciclos</a></li>
               <li class=""><a href="/academia/preinscripcion" class="{{ (Request::is('academia/preinscripcion')?'nav-item-active nav-item-noclick':'') }}">Preinscripción</a></li>
               <li class="nav-venues xs-hide" id="sedes">
@@ -75,6 +76,7 @@
               </li>
               <li class=""><a href="/academia/blog" class="{{ (Request::is('academia/blog*')?'nav-item-active':'') }}">Blog</a></li>
               <li class=""><a href="/academia/nosotros" class="{{ (Request::is('academia/nosotros')?'nav-item-active nav-item-noclick':'') }}">Nosotros</a></li>
+              <li class=""><a target="_blank" href="/academia-sorteo-laptop/">SORTEOS</a></li>
               <li class=""><a href="/academia/contacto" class="{{ (Request::is('academia/contacto')?'nav-item-active nav-item-noclick':'') }}">Contáctanos</a></li>
             </ul>
           </nav>

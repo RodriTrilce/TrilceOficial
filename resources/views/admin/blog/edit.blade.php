7e72658da7bf19f51514788b9fda315db509b67d
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-03-02 12:21:57 -0500

    Admin: arreglado visible, aprobado (solo para rol admin), marcado dentro de blog

--- resources/views/admin/blog/edit.blade.php
+++ resources/views/admin/blog/edit.blade.php
@@ -34,7 +34,7 @@
       <input type="checkbox" id="create_marker" name="marker" class="switch-input"
       {{ ($post->marker==1?'checked':'') }}
       >
-      <label for="create_state" class="switch-label">
+      <label for="create_marker" class="switch-label">
         Marcado (fijo):
         <span class="toggle--on">Si</span>
         <span class="toggle--off">No</span></label>      
@@ -46,7 +46,7 @@
         <input type="checkbox" id="create_approved" name="approved" class="switch-input"
         {{ ($post->approved==1?'checked':'') }}
         >
-        <label for="create_state" class="switch-label">
+        <label for="create_approved" class="switch-label">
           Aprobado:
           <span class="toggle--on">Si</span>
           <span class="toggle--off">No</span></label>      
@@ -58,7 +58,7 @@
       <input type="checkbox" id="create_draft" name="visible" class="switch-input"
       {{ ($post->visible==1?'checked':'') }}
       >
-      <label for="create_state" class="switch-label">
+      <label for="create_draft" class="switch-label">
         Visible:
         <span class="toggle--on">Si</span>
         <span class="toggle--off">No</span></label>      

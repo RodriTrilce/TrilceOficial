0c1c79f47fde865c801e44f8b0e6554f752859a7
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-04-10 08:30:15 -0500

    add tilde en guia de pagos

--- resources/views/colegio/payment_guide.blade.php
+++ resources/views/colegio/payment_guide.blade.php
@@ -17,7 +17,7 @@
 
       <div class="col-xs-12 playmentguide-title-container">
         <h1 class="playmentguide-title">Elija el banco de su preferencia</h1>
-        <p class="playmentguide-titlesub">Para saber como realizar pagos por internet, ventanilla y/o agente.</p>
+        <p class="playmentguide-titlesub">Para saber cómo realizar pagos por internet, ventanilla y/o agente.</p>
       </div>
 
       <div class="row col-xs-12 center-xs playmentguide-option">

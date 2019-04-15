dba3381517cddfcdea8e773a5f342d0e99f7ebf9
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-04-15 11:02:49 -0500

    delete test

--- resources/views/academia/partials/preparation/search/search-uni.blade.php
+++ resources/views/academia/partials/preparation/search/search-uni.blade.php
@@ -3,7 +3,6 @@
     <option value="0" selected hidden disable>Ciclos</option>
 
     <option value="0">Todos</option>
-    <option value="0">Not Data</option>
     <option value=30>Anual</option>
     <option value=35>Semestral</option>
 

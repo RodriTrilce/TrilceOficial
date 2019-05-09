70fa193c316c31444bdb82a04134c049688115f0
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-05-09 09:32:30 -0500

    olimpiadas js ok, response test

--- app/Http/Controllers/Academia/MathOlympicsRegisterController.php
+++ app/Http/Controllers/Academia/MathOlympicsRegisterController.php
@@ -71,7 +71,7 @@ class MathOlympicsRegisterController extends Controller
 		} catch ( SoapFault $e ) {
 			dd($e->getMessage());
 		}
-		//return view('/academia/index')->with(['page' => 'olympic']);
+		return view('/academia/index')->with(['page' => 'olympics']);
 		//return new OlympicsApiResource($collection);
 		//return view('/academia/simulacrum_exam_exito');
 

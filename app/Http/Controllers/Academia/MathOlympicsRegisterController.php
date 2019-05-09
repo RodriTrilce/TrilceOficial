325949061cc6b6553d031b2a7b9990c7cb3af3ab
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-05-09 12:33:45 -0500

    olimpiadas new none

--- app/Http/Controllers/Academia/MathOlympicsRegisterController.php
+++ app/Http/Controllers/Academia/MathOlympicsRegisterController.php
@@ -73,7 +73,7 @@ class MathOlympicsRegisterController extends Controller
 		}
 
 		return redirect()->route('academia-index')->with([
-			'olympics'  => true,
+			'contact'  => true,
 		  ]);
 		
 		//return new OlympicsApiResource($collection);

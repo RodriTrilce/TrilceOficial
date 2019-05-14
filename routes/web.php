017d6b3651ca6f67154a972dc3d86c7015907f98
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-05-14 12:47:43 -0500

    olimpiadas grupales v3

--- routes/web.php
+++ routes/web.php
@@ -157,7 +157,7 @@ Route::name('academia-matholympics-group-register')
       ->get('/academia/olimpiadas-matematicas/inscripcion-grupal/{codurl}', 'Academia\MathOlympicsRegisterController@indexGroup');
 
 Route::name('academia-matholympics-group-register-finish')
-      ->post('/academia/olimpiadas-matematicas/inscripcion/{codurl}', 'Academia\MathOlympicsRegisterController@store');
+      ->post('/academia/olimpiadas-matematicas/inscripcion-grupal/{codurl}', 'Academia\MathOlympicsRegisterController@store');
 
 
 

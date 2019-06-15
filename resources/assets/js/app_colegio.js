c64dd7bd08cdbc07f7d541da4b652943638912f5
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-06-15 14:09:09 -0500

    cokkie colegio.js ok

--- resources/assets/js/app_colegio.js
+++ resources/assets/js/app_colegio.js
@@ -179,6 +179,30 @@ if(page == 'index'){
     }
   }
 }
+//read and create cookie to popup
+
+function deleteCookie(){
+  log("eliminarCookie: " + key);
+  return document.cookie = key + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function getCookie(cname)
+{
+  var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+        return "";
+}
+
 
 /**!
 *  Contact

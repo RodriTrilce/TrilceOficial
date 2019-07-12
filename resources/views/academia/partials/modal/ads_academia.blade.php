8e75706bba8cbbd37f2fcf746e1d7179054d4dfb
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-07-12 17:01:00 -0500

    modif banner v2

--- resources/views/academia/partials/modal/ads_academia.blade.php
+++ resources/views/academia/partials/modal/ads_academia.blade.php
@@ -7,7 +7,7 @@
         	<div class="row col-xs-12">
               <div class="col-xs-12 col-sm-6">				
                 <a href="http://www.trilce.edu.pe/inscripcion-academia/" class="incriptions-acade-ads__link">
-                <h1><strong>¡No te quedes sin vacantes!</strong></h1>
+                <h1><strong>¡No te quedes sin vacante!</strong></h1>
                 <p>Nuevos ciclos disponibles</p>
                 <button class="incriptions-acade-ads__container-cta banner__button-cta banner__button-cta--white" type="button" name="button">
                   Regístrate aquí

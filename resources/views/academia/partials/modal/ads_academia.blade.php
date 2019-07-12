ae9be0241a4d528fe6c6d46628df83e7ef434594
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-07-12 16:49:10 -0500

    modif banner academia

--- resources/views/academia/partials/modal/ads_academia.blade.php
+++ resources/views/academia/partials/modal/ads_academia.blade.php
@@ -1,22 +1,25 @@
-<div class="row col-xs-12start-xs center-sm container-base ads-news-margins xs-hide">
-    <div class="row col-xs-12 col-sm-12 col-md-6 start-xs center-sm center-md">
+<div class="row col-xs-12 start-xs center-sm xs-hide container-main-aca" >
+      <div class="row col-xs-12 col-sm-9 col-md-8 start-xs start-sm start-md">
 
-      <div class="row col-xs-12 col-sm-12 col-md-4 center-xs incriptions-acade-ads">
-      <div class="row col-xs-12 col-sm-12 col-md-12 incriptions-acade-ads__container">
-            <a href="http://www.trilce.edu.pe/inscripcion-academia/" class="incriptions-acade-ads__link">
-                <h1><strong>¡No te quedes<br>sin vacantes!</strong></h1>
-                <p>
-                Nuevos ciclos disponibles
-                        </p>
+
+      <div class="row col-xs-12 center-xs incriptions-acade-ads">
+        <div class="incriptions-acade-ads__container">
+        	<div class="row col-xs-12">
+              <div class="col-xs-12 col-sm-6">				
+                <a href="http://www.trilce.edu.pe/inscripcion-academia/" class="incriptions-acade-ads__link">
+                <h1><strong>¡No te quedes sin vacantes!</strong></h1>
+                <p>Nuevos ciclos disponibles</p>
                 <button class="incriptions-acade-ads__container-cta banner__button-cta banner__button-cta--white" type="button" name="button">
                   Regístrate aquí
                 </button>
-            </a>
+                </a>
+              </div>
+              <div class="row col-xs-6 bottom-xs center-xs xs-hide">
+              <img src="{{ url('/static/images/academia/new-inscripciones-academia.png') }}" alt="">	
+              </div>        		
+        	  </div>
+          </div>
         </div>
+
       </div>
-      <div class="row col-xs-12 col-sm-6 col-md-4 bottom-xs center-xs xs-hide img-incription-ads">
-            <img src="{{ url('/static/images/academia/new-inscripciones-academia.png') }}" alt="">	
-      </div>
-        
     </div>
-</div>

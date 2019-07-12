8cb104e44e15300acad50e9461f3fdcc8afb8d45
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-07-12 08:08:03 -0500

    ads-academia arreglado

--- resources/views/academia/partials/modal/ads_academia.blade.php
+++ resources/views/academia/partials/modal/ads_academia.blade.php
@@ -1,7 +1,7 @@
 <div class="row col-xs-12start-xs center-sm container-base ads-news-margins xs-hide">
     <div class="row col-xs-12 col-sm-6 col-md-6 start-xs center-sm center-md">
 
-      <div class="row col-xs-12 col-sm-6 col-md-4 center-xs incriptions-acade-ads">
+      <div class="row col-xs-12 col-sm-12 col-md-4 center-xs incriptions-acade-ads">
       <div class="row col-xs-12 col-sm-12 col-md-12 incriptions-acade-ads__container">
             <a href="http://www.trilce.edu.pe/inscripcion-academia/" class="incriptions-acade-ads__link">
                 <h1><strong>¡No te quedes<br>sin vacantes!</strong></h1>

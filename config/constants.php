ca54f9ee43b333deae5e974c33be9511490fee4e
Cristhian Sanchez Mena <christhisam@gmail.com>
2019-08-02 17:48:30 -0500

    test v3

--- config/constants.php
+++ config/constants.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'API_ClientePublicoServicioLocal' => 'http://10.107.0.253:20169/General/ClientePublicoServicio.svc?wsdl',
-	'API_ClientePublicoServicioProduction' => 'http://10.107.0.253:20169/General/ClientePublicoServicio.svc?wsdl',
+	'API_ClientePublicoServicioLocal' => 'http://181.177.236.201/General/ClientePublicoServicio.svc?wsdl',
+	'API_ClientePublicoServicioProduction' => 'http://181.177.236.201/General/ClientePublicoServicio.svc?wsdl',
 ];
 
 /*return [

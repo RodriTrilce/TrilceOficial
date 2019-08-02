53fa4c2217d572444f235a9c2019ad748dbc5015
Cristhian Sanchez Mena <christhisam@gmail.com>
2019-08-02 17:46:07 -0500

    test v3

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
 	'API_ClientePublicoServicioLocal' => 'http://10.107.0.253:20169/General/ClientePublicoServicio.svc?wsdl',
-	'API_ClientePublicoServicioProduction' => 'http://181.177.236.201/General/ClientePublicoServicio.svc?wsdl',
+	'API_ClientePublicoServicioProduction' => 'http://10.107.0.253:20169/General/ClientePublicoServicio.svc?wsdl',
 ];
 
 /*return [

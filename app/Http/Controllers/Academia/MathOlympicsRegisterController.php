12442cd42ead09d0a593aadb4cc8ab2c34b34d90
Franco Salcedo <fsalcedo@trilce.edu.pe>
2019-08-05 14:55:32 -0500

    pdf grupal v5

--- app/Http/Controllers/Academia/MathOlympicsRegisterController.php
+++ app/Http/Controllers/Academia/MathOlympicsRegisterController.php
@@ -328,7 +328,7 @@ class MathOlympicsRegisterController extends Controller
 	}
 
 	public function imprimirPDFGROUP(Request $request){
-		dd($request->all());
+		//dd($request->all());
 		try {
 			$result = $this->client->OI_Dato([
 				'CODE_URL'  => $request->CODE_URL

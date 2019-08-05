2480bed62285c20881927c4174005b871989f497
Cristhian Sanchez Mena <christhisam@gmail.com>
2019-08-05 05:02:23 -0500

    tmp olympic v4

--- app/Http/Controllers/Academia/MathOlympicsRegisterController.php
+++ app/Http/Controllers/Academia/MathOlympicsRegisterController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers\Academia;
 
-define("_MPDF_TEMP_PATH", '/var/www/trilce.edu.pe/web/storage/framework/pdf/');
+//define("_MPDF_TEMP_PATH", '/var/www/trilce.edu.pe/web/storage/framework/pdf/');
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
